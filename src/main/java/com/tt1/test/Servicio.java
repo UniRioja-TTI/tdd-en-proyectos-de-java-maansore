a8966aedf908f29d5b7d19ad2981884e52292b7f
maansore <maansore@unirioja.es>
2026-02-26 14:39:28 +0100

    Clases final

--- src/main/java/com/tt1/test/Servicio.java
+++ src/main/java/com/tt1/test/Servicio.java
@@ -3,6 +3,14 @@ package com.tt1.test;
 import java.time.LocalDate;
 
 public class Servicio implements IServicio {
+
+    private IMailerStub mailer;
+    private IRepositorio repositorio;
+
+    public Servicio(IMailerStub mailer, IRepositorio repositorio) {
+        throw new UnsupportedOperationException("Clase aún no implementada.");
+    }
+
     @Override
     public void agnadirToDo(String nombre, LocalDate fechaLimite) {
         throw new UnsupportedOperationException("Clase aún no implementada.");
